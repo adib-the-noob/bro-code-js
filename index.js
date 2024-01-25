9619c77414f3dca232f342c4825ea601daaaabaf
adib-the-noob <mdadib550@gmail.com>
2024-01-25 07:26:46 +0600

    math added

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
-let firstName = "bro!";
-let age = 25;
-let isMarried = false;
+// let firstName = "bro!";
+// let age = 25;
+// let isMarried = false;
 
-document.getElementById("p1").innerHTML = "Hello " + firstName;
-document.getElementById("p2").innerHTML = "You are " + age + " years old!";
-document.getElementById("p3").innerHTML = "You are married: " + isMarried;
+// document.getElementById("p1").innerHTML = "Hello " + firstName;
+// document.getElementById("p2").innerHTML = "You are " + age + " years old!";
+// document.getElementById("p3").innerHTML = "You are married: " + isMarried;
 
-console.log(firstName);
-console.log(age);
+// console.log(firstName);
+// console.log(age);
 
-document.getElementById("mybtn").onclick = function () {
-    username = document.getElementById("username").value;
-    console.log(username);
+// document.getElementById("mybtn").onclick = function () {
+//     username = document.getElementById("username").value;
+//     console.log(username);
 
-    document.getElementById("mylabel").innerHTML = "Hello " + username;
-}
+//     document.getElementById("mylabel").innerHTML = "Hello " + username;
+// }
 
-// type conversion
-let userage = window.prompt("Enter your age: ");
-userage = Number(userage);
-console.log(userage + 1);
+// // type conversion
+// let userage = window.prompt("Enter your age: ");
+// userage = Number(userage);
+// console.log(userage + 1);
+
+// console.log(typeof userage);
+
+let x, y, z;
+x = 10;
+y = "pizza";
+z = 1;
+
+console.log(String(x));
+console.log(Number(y));
+console.log(Boolean(z));
---
+++ math.js
@@ -0,0 +1,19 @@
+let x = -2.933;
+
+// x = Math.pow(2 , 4)
+// x = Math.sqrt(64);
+// x = Math.abs(x);
+
+console.log(Math.round(x));
+
+
+let a, b , c;
+a = 10;
+b = 20;
+c = 30;
+
+max = Math.max(a, b, c);
+min = Math.min(a, b, c);
+
+console.log(max, min);
+console.log(Math.PI);
