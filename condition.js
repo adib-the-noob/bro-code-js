bbd0147e57d89b24e50f2f927ab615b7e1ca1c03
adib-the-noob <mdadib550@gmail.com>
2024-01-28 20:36:43 +0600

    checkbox added

---
+++ condition.js
@@ -0,0 +1,13 @@
+let age = 16;
+
+if (age >= 18) {
+    console.log("You are eligible to vote.");
+}
+
+else if (age >= 16) {
+    console.log("You are almost eligible to vote.");
+}
+
+else {
+    console.log("You are not eligible to vote.");
+}
---
+++ string-methods.js
@@ -0,0 +1,13 @@
+let userName = "ADIB_THE_NOOB";
+
+// console.log(userName.length);
+// console.log(userName.indexOf("T"));
+// console.log(userName.charAt(5));
+// console.log(userName.lastIndexOf("O"));
+
+let phoneNumber = "+880-1712-728225";    
+console.log(phoneNumber.replaceAll("-", ""));
+
+// method chaining
+let myName = "ADIB_THE_NOOB";
+myName = myName.toUpperCase().replace("_", " ").toLowerCase();
